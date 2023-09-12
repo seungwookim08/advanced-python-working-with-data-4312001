94a2f049cdca6848182fab825be68e3ac5a8f5f2
sw <seungwookim08@gmail.com>
2023-09-11 21:29:57 -0400

    ch45

--- Start/Ch_4/basiclog_start.py
+++ Start/Ch_4/basiclog_start.py
@@ -1,11 +1,20 @@
 # demonstrate the logging api in Python
 
 # TODO: use the built-in logging module
-
+import logging
 
 # TODO: Use basicConfig to configure logging
+logging.basicConfig(
+    level=logging.DEBUG, filename="Start\\Ch_4\\output.log", filemode="w"
+)
 
 # TODO: Try out each of the log levels
+logging.debug("Debug")
+logging.info("Info")
+logging.warning("Warning")
+logging.error("Error")
+logging.critical("Critical")
 
 # TODO: Output formatted strings to the log
-
+x = "test"
+logging.info(f"Formmated: {x}")
--- Start/Ch_4/customlog_start.py
+++ Start/Ch_4/customlog_start.py
@@ -3,13 +3,17 @@
 import logging
 
 # TODO: add another function to log from
-
+formatstr = "%(asctime)s: %(levelname)s: %(funcName)s Line:%(lineno)d %(message)s"
+datestr = "%m/%d/%Y %I:%M:%S %p"
 
 # set the output file and debug level, and
 # TODO: use a custom formatting specification
-logging.basicConfig(filename="output.log",
-                    level=logging.DEBUG)
+logging.basicConfig(
+    level=logging.DEBUG,
+    filename="Start\\Ch_4\\output2.log",
+    format=formatstr,
+    datefmt=datestr,
+)
 
 logging.info("This is an info-level log message")
 logging.warning("This is a warning-level message")
-

406ad8a218a7c86621b599608cb1bd6c4326802e
sw <seungwookim08@gmail.com>
2023-09-11 20:40:37 -0400

    defaultdict

--- Start/Ch_2/defaultdict.py
+++ Start/Ch_2/defaultdict.py
@@ -4,14 +4,21 @@ from collections import defaultdict
 
 
 # define a list of items that we want to count
-fruits = ['apple', 'pear', 'orange', 'banana',
-          'apple', 'grape', 'banana', 'banana']
+fruits = ["apple", "pear", "orange", "banana", "apple", "grape", "banana", "banana"]
 
 # TODO: use a dictionary to count each element
 fruitCounter = dict()
 
 # TODO: Count the elements in the list
 for fruit in fruits:
-    fruitCounter[fruit] += 1
+    if fruitCounter.get(fruit):
+        fruitCounter[fruit] += 1
+    else:
+        fruitCounter[fruit] = 1
+print(fruitCounter)
 
 # TODO: print the result
+dd = defaultdict(int)
+for fruit in fruits:
+    dd[fruit] += 1
+print(dd)

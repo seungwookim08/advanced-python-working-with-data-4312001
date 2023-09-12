3076376ba46ff5cddb87aab94350aef64c764acc
sw <seungwookim08@gmail.com>
2023-09-11 21:04:39 -0400

    serialize csv

--- Start/Ch_3/serialize_csv.py
+++ Start/Ch_3/serialize_csv.py
@@ -6,7 +6,7 @@ import json
 import datetime
 
 # read in the contents of the JSON file
-with open("../../30DayQuakes.json", "r") as datafile:
+with open("30DayQuakes.json", "r") as datafile:
     data = json.load(datafile)
 
 
@@ -19,7 +19,16 @@ def isbig(x):
 largequakes = list(filter(isbig, data["features"]))
 
 # TODO: Create the header and row structures for the data
-
+header = ["Place", "Magnitude", "Link", "Date"]
+rows = []
 # TODO: populate the rows with the resulting quake data
+for q in largequakes:
+    d = datetime.date.fromtimestamp(q["properties"]["time"] // 1000)
+    p = q["properties"]
+    rows.append([p["place"], p["mag"], p["url"], d])
 
 # TODO: write the results to the CSV file
+with open(".\\Start\\Ch_3\\largequakes.csv", "w", newline="") as c:
+    writer = csv.writer(c, delimiter=",")
+    writer.writerow(header)
+    writer.writerows(rows)

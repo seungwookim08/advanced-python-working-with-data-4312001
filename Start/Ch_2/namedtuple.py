6983fe5e74fd2bbf3431108de6faa0f626b2b83f
sw <seungwookim08@gmail.com>
2023-09-11 20:38:46 -0400

    named tuple

--- Start/Ch_2/namedtuple.py
+++ Start/Ch_2/namedtuple.py
@@ -4,5 +4,11 @@ import collections
 
 
 # TODO: create a Point namedtuple
+Point = collections.namedtuple("Point", "x y")
+p = Point(1, 3)
+print(p)
+print(p.x, p.y)
 
 # TODO: use _replace to create a new instance
+p = p._replace(x=100)
+print(p)

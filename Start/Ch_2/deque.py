14c031612eaca62f9f609c7daf04ba4602d5e65f
sw <seungwookim08@gmail.com>
2023-09-11 20:46:27 -0400

    deque

--- Start/Ch_2/deque.py
+++ Start/Ch_2/deque.py
@@ -5,11 +5,20 @@ import string
 
 
 # TODO: initialize a deque with lowercase letters
+d = collections.deque(string.ascii_lowercase)
 
 # TODO: deques support the len() function
+print("num of d", len(d))
 
 # TODO: deques can be iterated over
+print(list(i for i in d))
 
 # TODO: manipulate items from either end
+d.pop()
+d.popleft()
+d.append(2)
+d.appendleft(1)
+print(d)
 
 # TODO: use an index to get a particular item
+print(d[0])
